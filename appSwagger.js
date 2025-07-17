5ee6b783bfc7ac756ac20c472bcc84f3a225b484
Abdal Hussain <abdalhussain5790@gmail.com>
2025-07-17 09:56:22 +0500

    Add Link

--- appSwagger.js
+++ appSwagger.js
@@ -20,7 +20,8 @@ const swaggerOptions = {
       {
         // url: 'https://eb-project-backend-seven.vercel.app',
         // url: 'https://eb-project-backend-pied.vercel.app',
-        url: 'https://eb-project-backend-production-ca3b.up.railway.app',
+        // url: 'https://eb-project-backend-production-ca3b.up.railway.app',
+        url: 'https://planty-backend-production.up.railway.app',
       },
     ], 
   }, 
