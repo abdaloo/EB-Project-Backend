555bd5ba64a71d6d0933bc501dfa03d37dca939e
Abdal Hussain <abdalhussain5790@gmail.com>
2025-07-18 16:57:30 +0500

    Create file for cloudinary

---
+++ config/cloudinaryConfig.js
@@ -0,0 +1,23 @@
+// cloudinaryConfig.js
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const multer = require('multer');
+require('dotenv').config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'plants', // Cloudinary folder
+    allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = { upload };
--- routes/PlantRoute.js
+++ routes/PlantRoute.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const plantController = require('../controllers/PlantController');
+const { upload } = require('../config/cloudinaryConfig');
+
+router.post('/uploadImage', upload.single('image'), (req, res) => {
+  res.json({
+    message: 'Image uploaded successfully!',
+    imageUrl: req.file.path,
+  });
+});
 
 // Plant Image routes
-router.post('/upload-image', plantController.uploadPlantImage);
+// router.post('/upload-image', plantController.uploadPlantImage);
 
 // Plant routes
 router.post('/create', plantController.createPlant);
